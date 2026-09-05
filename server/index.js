4ffb0c0f78bbc0f003373654b67082ca4745d9c6
Manas <manas.somnathindore2024@vitstudent.ac.in>
2026-09-05 14:44:50 +0530

    fix: add vercel domain to cors allowed origins

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://interview-iq-gamma-ruby.vercel.app/']
+  origin: ['http://localhost:5173', 'https://interview-iq-gamma-ruby.vercel.app']
 }));
 app.use(express.json());
 
